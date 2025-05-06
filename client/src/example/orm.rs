9d4119736840ca2e07e052e8069503aef00654d8
913530621@qq.com <wang913530621@gmail.com>
2025-05-06 18:58:42 +0900

    add demo

---
+++ client/src/example/orm.rs
@@ -0,0 +1,23 @@
+mod entities;
+use chrono::Utc;
+use dotenvy::dotenv;
+use entities::{prelude::*, *};
+use sea_orm::*;
+use uuid::Uuid;
+async fn demo() -> Result<(), anyhow::Error> {
+    dotenv().ok();
+    let url = std::env::var("DATABASE_URL").expect("can'T get the database URL");
+    let db = Database::connect(url).await.expect("");
+    let happy_bakery = users::ActiveModel {
+        id: Set(Uuid::new_v4().as_bytes().to_vec()),
+        username: Set("ABCD".to_owned()),
+        password_hash: Set("...hashed...".to_owned()),
+        email: Set("...hashed...".to_owned()),
+        created_at: Set(Some(Utc::now())),
+    };
+    Users::insert(happy_bakery).exec(&db).await.expect("msg");
+
+    let res = users::Entity::find().one(&db).await.expect("msg");
+    dbg!(res);
+    Ok(())
+}
